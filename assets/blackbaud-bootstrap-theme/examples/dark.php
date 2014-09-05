a4e32ebebf64800108bbdbcce56402470d1eb455
Bobby Earl <bobby.earl@chs7bobbyear02.blackbaud.com>
2014-09-05 12:08:03 -0400

    Layout updates

---
+++ assets/blackbaud-bootstrap-theme/examples/dark.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Blackbaud</title>
+
+  <link rel="icon" href="../img/favicon.ico" type="image/ico">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../lib/owl-carousel/assets/owl.carousel.min.css">
+  <link rel="stylesheet" href="../lib/owl-carousel/assets/owl.theme.default.min.css">
+  <link rel="stylesheet" href="../lib/animate/animate.css">
+  <link rel="stylesheet" href="../css/blackbaud-bootstrap-theme.css">
+
+</head>
+<body>
+
+  <nav class="navbar navbar-inverse" role="navigation">
+    <div class="container">
+
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bb-navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="http://www.blackbaud.com" target="_blank" class="navbar-brand">
+          <img src="../img/logo-white.png" alt="Blackbaud" />
+        </a>
+      </div>  <!-- navbar-header -->
+
+      <div class="collapse navbar-collapse" id="bb-navbar">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="navbar-text">Example Text</li>
+          <li class="vertical-divider">&nbsp;</li>
+          <li><a href="#"><i class="fa fa-cogs"></i> Sample Link</a></li>
+          <li class="vertical-divider">&nbsp;</li>
+          <li><a href="#"><i class="fa fa-question-circle"></i></a></li>
+        </ul>  <!-- .navbar-nav -->
+      </div>  <!-- .navbar-collapse -->
+
+    </div>  <!-- .container -->
+  </nav>  <!-- .navbar.navbar-inverse -->
+
+  <div class="header">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          
+          <h1><a href="./">Project Title</a></h1>
+          <h2>Project Tagline</h2>
+          
+        </div>  <!-- .col-md-6 -->
+        <div class="col-md-6">
+          <div class="status-wrapper text-right">
+            Project Status
+          </div>  <!-- .status-wrapper -->
+        </div>  <!-- .col-md-6 -->
+      </div>  <!-- .row -->
+
+      <div class="row hide">
+        <div class="col-sm-12">
+          <ul class="nav nav-tabs">
+            <li class="active">
+              <a href="#tab1" data-toggle="tab">
+                Example Tab 1</span>
+              </a>
+            </li>
+            <li>
+              <a href="#tab2" data-toggle="tab">
+                Example Tab 2
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>  <!-- .row -->
+      
+    </div>  <!-- .container -->
+  </div>  <!-- .header -->
+
+  <div class="content">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <h3>Create Custom Parts</h3>
+          <h3>Intergrate with Blackbaud CRM &#8482;</h3>
+          <h3>Integrate with Blackbaud Secure Payments</h3>
+        </div>  <!-- .col-xs-12 -->
+      </div>  <!-- .row -->
+    </div>  <!-- .container -->
+  </div>  <!-- .content -->
+
+  <div class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          Blackbaud
+        </div>  <!-- .col-sm-12 -->
+      </div>  <!-- .row -->
+    </div>  <!-- .container -->
+  </div>  <!-- .footer -->
+
+  <div class="copyright">
+    <div class="container">
+      <div class="col-sm-12">
+        &copy; Blackbaud 2014
+      </div>
+    </div>
+  </div>  <!-- .copyright -->
+
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/gsap/1.13.1/TweenMax.min.js"></script>
+  <script src="../lib/owl-carousel/owl.carousel.js"></script>
+  <script src="../js/owl.feature.js"></script>
+  <script src="../js/blackbaud-bootstrap-theme.js"></script>
+  
+</body>
+</html>
