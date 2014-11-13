8123e8be317b2093be268e612169910ebcbb6a81
Bobby Earl <bobby.earl@blackbaud.com>
2014-11-13 11:04:31 -0500

    Removing copy button for mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,24 +90,35 @@
         var client = new ZeroClipboard(copyCode);
         var bridge;
         
-        client.on('ready', function() {
-          bridge = $('#global-zeroclipboard-html-bridge');
-          bridge.data('placement', 'left').data('trigger', 'manual').attr('title', 'Copied!').tooltip();
-        });
-        
+        // Show the tooltip
         copyCode.on('mouseover', function() {
           $(this).tooltip('show');
         });
         
+        // Hide the tooltip and our manually created one in case it was open
         copyCode.on('mouseout', function() {
           $(this).tooltip('hide');
           bridge.tooltip('hide');
         });
         
+        // When the SWF is ready, add our manually triggered tooltip
+        client.on('ready', function() {
+          bridge = $('#global-zeroclipboard-html-bridge');
+          bridge.data('placement', 'left').data('trigger', 'manual').attr('title', 'Copied!').tooltip();
+        });
+        
+        // This usually happens on mobile devices when the SWF doesn't or can't load.
+        // We'll just hide the button in that case.
+        client.on('error', function() {
+          copyCode.hide();
+        });
+        
+        // Copy the related content to the clipboard
         client.on('copy', function(e) {
           e.clipboardData.setData('text/plain', $(e.target).siblings('.highlight').text());
         });
         
+        // Show the copied tooltip
         client.on('aftercopy', function(e) {
           copyCode.tooltip('hide');
           bridge.tooltip('show');
