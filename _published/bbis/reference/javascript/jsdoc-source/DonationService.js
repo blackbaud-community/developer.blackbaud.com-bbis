583537291d6823cd553f7a5b8d5256b3a1547e83
Bobby Earl <bobby.earl@blackbaud.com>
2017-07-03 09:56:18 -0400

    Latest Release

--- _published/bbis/reference/javascript/jsdoc-source/DonationService.js
+++ _published/bbis/reference/javascript/jsdoc-source/DonationService.js
@@ -182,6 +182,136 @@
                 type: 'GET'
             }, this._rpc, successCallback, failureCallback);
         }
+		
+		 /**
+        * Calculates the price on each installment of a recurring gift based on how many installments
+        * the client wants to pay. If empty, zero, or negative parameters are supplied, it returns 0.
+        * @param {Decimal} totalGiftAmount The total amount of the gift that the client wishes to pay
+        * @param {Integer} numberOfInstallments The number of installments that the client would like to pay the full price of the gift
+        */
+        getRecurringGiftInstallmentAmount: function (totalGiftAmount, numberOfInstallments) {
+            // Format the parameters to be a decimal and integer
+            var total = 0,
+                installmentCount = 0,
+                installmentAmount = 0;
+            total = parseFloat(totalGiftAmount).toFixed(2);
+            installmentCount = parseInt(numberOfInstallments, 10);
+
+            // If any empty parameter is supplied, we return 0
+            if (!total || !installmentCount || total <= 0 || installmentCount <= 0) {
+                return 0;
+            }
+            // Calculate the installment amount by dividing the total by the number of installments (we floor it to a whole number just in case someone supplies a decimal)
+            installmentAmount = total / Math.floor(installmentCount);
+            // Return the calculated value as it is rounded to the 100th decimal place to match currency
+            return installmentAmount.toFixed(2);
+        },
+
+        /**
+        * Calculates the end date of a recurring installment payment based on the number the of installments desired to pay the recurring gift or pledge.
+        * @param {integer} numberOfInstallments The number of installments the user wants to pay off their pledge or recurring gift in
+        * @param {integer} frequencyCode The integer value representing the frequency of the user's installments (monthly: 2, quarterly: 3, annually: 4)
+        * @param {date} installmentStartDate The date that the first installment will be paid on
+        * @param {integer} installmentMonth The month that the installment payment will occur if it is an annual payment (January: 1, Febraury: 2, March: 3, etc.)
+        * @param {integer} installmentDayOfMonth The date within a month that the installment payment will occur if it is a monthly or annual payment
+        */
+        getRecurringGiftLastPaymentDate: function (numberOfInstallments, frequencyCode, installmentStartDate, installmentMonth, installmentDayOfMonth) {
+            // VARIABLE SET UP: Make the variables the types of objects that we want to use
+            // NOTE: All of the vars are established here to meet JSLint standards
+            var installmentCount = 0,
+                frequency = 0,
+                startDate,
+                endDate,
+                month = 0,
+                dayOfMonth = 0,
+                monthProvided = false,
+                dayOfMonthProvided = false,
+                startDateMonth = 0,
+                startDateDayOfMonth = 0,
+                startDateYear = 0,
+                dateNotAvailableUntilNextYear = 0;
+            // Because the first installment is on the start date, we subtract 1 installment from the number of installments lef ton the payment
+            installmentCount = parseInt(numberOfInstallments, 10) - 1;
+            // Cast the frequency as an integer in case they passed in as a string
+            frequency = parseInt(frequencyCode, 10);
+            // Grab the start date as date variable
+            startDate = new Date(installmentStartDate);
+            // Set the end date as the start date for now since that will be our starting point
+            endDate = startDate;
+            // Because Javascript zero-indexes months (January: 0, February: 1, etc.) but the Blackbaud donation processing API code does not, we tell the user to provide a non-zero-indexed month and we subtract 1 from it
+            month = parseInt(installmentMonth, 10) - 1;
+            // Cast the day of month as an integer in case they passed it in as a string
+            dayOfMonth = parseInt(installmentDayOfMonth, 10);
+
+            // VALIDATION: Validate everything possible before continuing because we never know what could happen with particular parameters
+            // if the number of installments provided is undefined or 0, we exit the function
+            if (!installmentCount || installmentCount <= 0) {
+                return;
+            }
+            // if the frequency is undefined or outside of the expected range(2,3,4), we exit the function
+            if (!frequency || 2 > frequency || frequency > 4) {
+                return;
+            }
+            // if the start date is undefined, we exit the function
+            // else validate that the start date we're about to calculate with is an actual date (for example: April 31st does not exist) and exit the function if necessary
+            if (!startDate) {
+                return;
+            }
+
+            // START DATE ADJUSTMENT: if the installment frequency does not coicinde with the start date, we need to adjust the start date before determing an end date
+            // the month is considered "provided" if it is provided and is in the range of 1 to 12
+            monthProvided = (month && 0 < month && month < 13);
+            // the day of month is considered "provided" if it is provided and is in the range of 1 to 31
+            dayOfMonthProvided = (dayOfMonth && 0 < dayOfMonth && dayOfMonth < 32);
+
+            // if the month is provided without a day, we can't work with it so we exit the function
+            // else if the month is provided then we're working with an annual date that we may need to adjust
+            // else if just the day of month is provided then we're working with a monthly or quarterly date that we may need to adjust
+            if (monthProvided && !dayOfMonthProvided) {
+                return;
+            } else if (monthProvided) {
+                // get the start date's month, day, and year
+                startDateMonth = startDate.getMonth();
+                startDateDayOfMonth = startDate.getDate();
+                startDateYear = startDate.getFullYear();
+                // if the installment's annual month is before the start date's month, it is not available until next year
+                // OR if the installment's annul month is the same month as the installment month but the installment date is before the start date's date, that date is not available until next year
+                dateNotAvailableUntilNextYear = (month < startDateMonth) || (month === startDateMonth && dayOfMonth < startDateDayOfMonth);
+                if (dateNotAvailableUntilNextYear) {
+                    // because the month is not available until next year, we up the start date by 1 year
+                    startDate.setYear(startDateYear + 1);
+                }
+                // set the start date's day and month to the expected day and month
+                startDate.setDate(dayOfMonth);
+                startDate.setMonth(month);
+            } else if (dayOfMonthProvided) {
+                // if the installment's day of month is before the start date's day of month, it is not available until next month
+                if (dayOfMonth < startDate.getDate()) {
+                    // because the start date is not available until next month, we up the start date by 1 month
+                    startDate.setMonth(startDate.getMonth() + 1);
+                }
+                // we set the start date's date to match the installment's day of month
+                startDate.setDate(dayOfMonth);
+            }
+
+            // END DATE CALCULATION: depending on the frequency, we add the number of installments in months, quaters, or years to the start date to determine the end date
+            switch (frequency) {
+            case 2: // monthly
+                // 1 installment = 1 month
+                endDate.setMonth(startDate.getMonth() + installmentCount);
+                break;
+            case 3: // quaterly
+                // 1 installment = 3 months
+                endDate.setMonth(startDate.getMonth() + (3 * installmentCount));
+                break;
+            case 4: // annually
+                // 1 installment = 1 year
+                endDate.setYear(startDate.getFullYear() + installmentCount);
+                break;
+            }
+
+            return endDate;
+        }
 
     };
 
